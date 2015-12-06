67b6a6858aa0093bf9f0c96753c52708298d6dbf
ZLinuxFan <zlinuxfan@gmail.com>
2015-12-06 11:45:10 +0200

    Используем openCV для создания фильтра контуров изображения

---
+++ labTensorFlow/filterCircuitsCV.py
@@ -0,0 +1,12 @@
+#!/usr/bin/evn python3
+
+import cv2 as cv
+import sys
+
+img = cv.imread(sys.argv[1], 1)
+cv.imshow("original", img)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+gray = cv.GaussianBlur(gray, (7, 7), 1.5)
+edges = cv.Canny(gray, 0, 50)
+cv.imshow("edges", edges)
+cv.waitKey()
