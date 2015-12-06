502df1fc453ef4b0b20e23a8e38e4125d03cc0ea
ZLinuxFan <zlinuxfan@gmail.com>
2015-12-06 11:48:38 +0200

    Добавлен фильтр контуров

--- labTensorFlow/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/evn python3
-print("Hello world!")
-
-import math
-
-a = int(input("Введите первый катет: "))
-b = int(input("Введите второй катет: "))
-c = math.sqrt(a ** 2 + b ** 2)
-print("Длина гипотенузы состовляет: " + c)
